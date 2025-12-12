c9581a720ab51a70ad191df7432b44487c0f082b
Nav0za <thanaphat.tn@rmuti.ac.th>
2025-12-12 14:24:17 +0700

    delete old table and api

--- server/api/schedule/index.get.js
+++
@@ -1,7 +0,0 @@
-import db from '../../utils/db';
-
-export default defineEventHandler(async (event) => {
-    const stmt = db.prepare('SELECT * FROM schedule ORDER BY scheduleId')
-    const schedules = stmt.all()
-    return schedules
-})
