c2eb41bd2fe1e4d92107d0ebf642a3169924cc48
Nav0z <thanaphat.tn@rmuti.ac.th>
2025-12-23 16:26:34 +0700

    แก้ api + เปลี่ยนช่องตารางเป็น USelect

--- server/api/Subjects/index.get.js
+++ server/api/Subjects/index.get.js
@@ -1,7 +1,18 @@
 import db from '../../utils/db.js'
 
-export default defineEventHandler(() => {
-    const stmt = db.prepare('SELECT * FROM Subjects')
-    const subjects = stmt.all()
+export default defineEventHandler((event) => {
+    const query = getQuery(event)
+    const { id_teacher } = query
+
+    let stmt
+    let subjects
+    // เช็คว่ามี id teacher มั้ย
+    if (id_teacher) {
+        stmt = db.prepare('SELECT * FROM Subjects WHERE id_teacher = ?')
+        subjects = stmt.all(id_teacher)
+    } else {
+        stmt = db.prepare('SELECT * FROM Subjects')
+        subjects = stmt.all()
+    }
     return subjects
 })
