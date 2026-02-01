b872548e6dc14e470b37db40c5a1d3e3be328e54
takzx456 <phaphop.tak@gmail.com>
2026-02-01 21:20:42 +0700

    4x5 table ตึก 13

---
+++ app/pages/formterm/13.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-lg font-semibold mb-4">ตาราง 4x5</h1>
+    <FormTerm13 />
+    <div class="mt-4">
+      <NuxtLink to="/" class="no-underline">
+        <UButton label="กลับ" color="neutral" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import FormTerm13 from '~/components/FormTerm/13.vue'
+</script>
